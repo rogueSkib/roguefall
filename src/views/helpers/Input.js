8f569c3fb4eb59aea69796dbfb8f259b79532176
Jimmy Griffith <rogueSkib@gmail.com>
2013-10-28 03:27:13 -0700

    * working on horizontal movement

--- src/views/helpers/Input.js
+++ src/views/helpers/Input.js
@@ -2,8 +2,8 @@ import ui.View as View;
 
 exports = Class(View, function(supr) {
 
-	var JUMP_PX_PER_MS = 0.25,
-		JUMP_PX_MIN = -40;
+	var JUMP_PX_PER_MS = 0.1,
+		JUMP_PX_MIN = -30;
 
 	var gameView;
 
@@ -21,7 +21,7 @@ exports = Class(View, function(supr) {
 		if (this.inputStartEvt === null) {
 			this.inputStartEvt = evt;
 			this.inputStartPt = pt;
-			gameView.player.targetX = pt.x;
+			gameView.player.setTargetX(pt.x);
 		}
 	};
 
@@ -31,7 +31,7 @@ exports = Class(View, function(supr) {
 
 		if (!this.movedThisStep && startEvt && evt.id === startEvt.id) {
 			this.movedThisStep = true;
-			gameView.player.targetX = pt.x;
+			gameView.player.setTargetX(pt.x);
 
 			if (!this.hasJumped) {
 				var dx = pt.x - startPt.x,
@@ -46,7 +46,7 @@ exports = Class(View, function(supr) {
 	};
 
 	this.onInputSelect = function(evt, pt) {
-		if (evt.id === this.inputStartEvt.id) {
+		if (this.inputStartEvt && evt.id === this.inputStartEvt.id) {
 			this.inputStartEvt = null;
 			this.inputStartPt = null;
 			this.hasJumped = false;
--- src/views/helpers/Player.js
+++ src/views/helpers/Player.js
@@ -10,13 +10,16 @@ exports = Class(View, function(supr) {
 		BG_HEIGHT,
 		PLAYER_WIDTH = 164,
 		PLAYER_HEIGHT = 164,
-		PLAYER_FEET = 130,
+		PLAYER_FEET = 154,
 		GRAVITY = 0.003,
 		AIR_RESISTANCE = 0.0032,
 		WALL_RESISTANCE = 0.0064,
 		WALL_WIDTH = 56,
-		JUMP_ACCEL = -0.0125,
-		JUMP_ACCEL_TIME = 200;
+		JUMP_ACCEL = -0.025,
+		JUMP_ACCEL_TIME = 100,
+		RUN_SPEED_MAX = 0.8,
+		RUN_ACCEL_MAX = 0.008,
+		RUN_DECEL_RANGE = 100;
 
 	var STATES = {},
 		STATE_IDLE = 0,
@@ -84,7 +87,7 @@ exports = Class(View, function(supr) {
 			canHandleEvents: false
 		});
 
-		this.boundJumpAccelEnd = bind(this, function() {
+		this.boundAllowGravity = bind(this, function() {
 			this.ignoreGravity = false;
 		});
 	};
@@ -99,6 +102,9 @@ exports = Class(View, function(supr) {
 		this.axAnim = animate(this.a, 'ax');
 		this.ayAnim = animate(this.a, 'ay');
 		this.ignoreGravity = false;
+		this.hasLanded = false;
+		this.hasJumped = false;
+		this.hasDoubleJumped = false;
 		this.setState(STATE_DEFAULT);
 	};
 
@@ -112,17 +118,25 @@ exports = Class(View, function(supr) {
 	};
 
 	this.jump = function() {
-		if (this.state !== STATE_JUMPING
-			&& this.state !== STATE_FALLING)
-		{
-			this.setState(STATE_JUMPING);
-			this.ignoreGravity = true;
-			this.a.y = 0;
-
-			this.ayAnim.now({ y: JUMP_ACCEL }, JUMP_ACCEL_TIME / 2, animate.easeOut)
-			.then({ y: 0 }, JUMP_ACCEL_TIME / 2, animate.easeOut)
-			.then(this.boundJumpAccelEnd);
+		if (this.hasJumped && !this.hasDoubleJumped) {
+			this.hasDoubleJumped = true;
+		} else if (!this.hasJumped) {
+			this.hasJumped = true
+		} else {
+			return;
 		}
+
+		if (this.state === STATE_WALL_SLIDING) {
+			this.hasDoubleJumped = true;
+		}
+
+		this.setState(STATE_JUMPING);
+		this.ignoreGravity = true;
+		this.a.y = 0;
+
+		this.ayAnim.now({ y: JUMP_ACCEL }, JUMP_ACCEL_TIME / 2, animate.easeOut)
+		.then({ y: 0 }, JUMP_ACCEL_TIME / 2, animate.easeOut)
+		.then(this.boundAllowGravity);
 	};
 
 	this.finishLanding = function() {
@@ -130,25 +144,65 @@ exports = Class(View, function(supr) {
 		this.setState(STATE_IDLE);
 	};
 
+	this.setTargetX = function(x) {
+		this.targetX = x;
+	};
+
 	this.step = function(dt) {
+		var abs = Math.abs;
+
 		if (this.targetX <= WALL_WIDTH) {
 			this.targetX = WALL_WIDTH / 2;
 		} else if (this.targetX >= BG_WIDTH - WALL_WIDTH) {
 			this.targetX = BG_WIDTH - WALL_WIDTH / 2;
 		}
 
+		var dx = this.targetX - this.x,
+			runSign = dx >= 0 ? 1 : -1;
+
 		// flip player x based on direction
-		if (this.targetX < this.x && !this.flippedX) {
+		if (dx < 0 && !this.flippedX) {
 			this.flippedX = this.style.flipX = true;
-		} else if (this.targetX > this.x && this.flippedX) {
+		} else if (dx > 0 && this.flippedX) {
 			this.flippedX = this.style.flipX = false;
 		}
 
-		this.x = (this.targetX + 7 * this.x) / 8;
+		// slow down closer to target
+		var decelMult = 1;
+		if (dx && abs(dx) < RUN_DECEL_RANGE) {
+			decelMult = 0.01 + abs(dx) / RUN_DECEL_RANGE;
+		}
+
+		// horizontal movement
+		var startX = this.x;
+		this.x += dt * decelMult * this.v.x / 2;
+		this.v.x += dt * this.a.x / 2;
+		if (this.x !== this.targetX && this.v.x !== runSign * RUN_SPEED_MAX) {
+			this.a.x = runSign * RUN_ACCEL_MAX;
+		}
+		this.v.x += dt * this.a.x / 2;
+		if (abs(this.v.x) > RUN_SPEED_MAX) {
+			this.v.x = runSign * RUN_SPEED_MAX;
+			this.a.x = 0;
+		}
+		this.x += dt * decelMult * this.v.x / 2;
+		if (startX <= this.targetX && this.x >= this.targetX) {
+			this.x = this.targetX;
+			this.v.x = 0;
+			this.a.x = 0;
+		} else if (startX >= this.targetX && this.x <= this.targetX) {
+			this.x = this.targetX;
+			this.v.x = 0;
+			this.a.x = 0;
+		} else if (this.x < WALL_WIDTH / 2) {
+			this.x = WALL_WIDTH / 2;
+		} else if ( this.x > BG_WIDTH - WALL_WIDTH / 2) {
+			this.x = BG_WIDTH - WALL_WIDTH / 2;
+		}
 		this.style.x = this.x - this.width / 2;
 
 		var resistance = AIR_RESISTANCE;
-		if (this.x <= WALL_WIDTH || this.x >= BG_WIDTH - WALL_WIDTH) {
+		if (!this.ignoreGravity && (this.x <= WALL_WIDTH || this.x >= BG_WIDTH - WALL_WIDTH)) {
 			resistance = WALL_RESISTANCE;
 
 			// force opposite flip on walls
@@ -159,24 +213,33 @@ exports = Class(View, function(supr) {
 			}
 		}
 
+		// vertical movement
 		var startY = this.y;
 		this.y += dt * this.v.y / 2;
 		this.v.y += dt * this.a.y / 2;
 		!this.ignoreGravity && (this.a.y = GRAVITY - this.v.y * resistance);
 		this.v.y += dt * this.a.y / 2;
 		this.y += dt * this.v.y / 2;
-		this.falling = !this.checkPlatformCollision(startY);
+		this.falling = !this.checkPlatformCollision(startY) && this.v.y > 0;
 
 		if (resistance !== WALL_RESISTANCE) {
 			if (this.falling) {
 				this.setState(STATE_FALLING);
-			} else if (!~~Math.abs(this.targetX - this.x)) {
+			} else if (this.hasLanded && !~~abs(this.targetX - this.x)) {
 				this.setState(STATE_IDLE);
-			} else {
+			} else if (this.hasLanded) {
 				this.setState(STATE_RUNNING);
 			}
 		} else if (this.falling) {
 			this.setState(STATE_WALL_SLIDING);
+			this.hasJumped = false;
+			this.hasDoubleJumped = false;
+		} else {
+			if (this.hasLanded && !~~abs(this.targetX - this.x)) {
+				this.setState(STATE_IDLE);
+			} else if (this.hasLanded) {
+				this.setState(STATE_RUNNING);
+			}
 		}
 
 		return this.y;
@@ -199,10 +262,14 @@ exports = Class(View, function(supr) {
 					this.y = platY - PLAYER_FEET;
 					this.v.y = 0;
 					this.a.y = 0;
+					this.hasLanded = true;
+					this.hasJumped = false;
+					this.hasDoubleJumped = false;
 					return true;
 				}
 			}
 		}
+		this.hasLanded = false;
 		return false;
 	};
 });
